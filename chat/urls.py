79e64fdb9ba50ea22583f486db9f3c89fad15885
k2dums <norbu.paddu@gmail.com>
2023-01-10 22:33:31 +0530

    added url path for the chat app

--- chat/urls.py
+++ chat/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path 
-
+from . import views
 urlpatterns=[
-    path(''),
+  path('',views.index_view,name='index'),
+  path('register/',views.register_view,name='register'),
+  path('login/',views.login_view,name='login')
 ]
 
