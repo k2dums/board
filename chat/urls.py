b43fe0dff4b8001a9dac74c8602eade86d92c084
k2dums <norbu.paddu@gmail.com>
2023-01-11 17:16:36 +0530

    added appname and path logout

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path 
 from . import views
+
+app_name='chat'
 urlpatterns=[
   path('',views.index_view,name='index'),
   path('register/',views.register_view,name='register'),
-  path('login/',views.login_view,name='login')
+  path('login/',views.login_view,name='login'),
+  path('logout/',views.logout_view,name='logout')
 ]
 
