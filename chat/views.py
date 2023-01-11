bddfbab41eb3e2ae7a3f6dfbc5935f378f03b675
k2dums <norbu.paddu@gmail.com>
2023-01-11 17:17:43 +0530

    added the appname in the path redirects

--- chat/views.py
+++ chat/views.py
@@ -8,7 +8,7 @@ from django.db import IntegrityError
 # Create your views here.
 def index_view(request):
     if not(request.user.is_authenticated):
-        return HttpResponseRedirect(reverse('login'))
+        return HttpResponseRedirect(reverse('chat:login'))
 
     return render(request,"chat/chat.html",{})
 
@@ -16,13 +16,14 @@ def index_view(request):
 def register_view (request):
     if request.method=='POST':
         #Get the user field from the form
-        username=request.POST.get['username']
-        email=request.POST.get['email']
-        password=request.POST.get['password']
-        confirm_password=request.get['confirm_password']
+        username=request.POST.get('username')
+        email=request.POST.get('email')
+        password=request.POST.get('password')
+        confirm_password=request.POST.get('confirm_password')
         if password!=confirm_password:
             return render(request,'chat/register.html',{'message':"Passwords must match"})
-            
+        #Need to make sure the fields have valid values
+
         #Attempt to create new user
         try:
             user=User.objects.create_user(username,email,password)
@@ -32,21 +33,21 @@ def register_view (request):
                 'messsage':'Username already taken'
             })
         login(request,user)
-        return HttpResponseRedirect(reverse('index'))
-        
+        return HttpResponseRedirect(reverse('chat:index'))
+
     else:#For GET/PUT....
         return render(request,'chat/register.html',{})
 
 def login_view(request):
     if request.method=='POST':
-        username=request.POST.get['username']
-        password=request.POST.get['password']
+        username=request.POST.get('username')
+        password=request.POST.get('password')
 
         user=authenticate(request,username=username,password=password)
 
         if user is not None:#Authenticated
             login(request,user)
-            return HttpResponseRedirect(reverse('index'))
+            return HttpResponseRedirect(reverse('chat:index'))
         
         else: #Not authenticated
             return render(request,'chat/login.html',{'message':'Invalid username or password'})
@@ -56,4 +57,4 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     messages.info(request,'You have been logged out')
-    return HttpResponseRedirect(reverse('index'))
+    return HttpResponseRedirect(reverse('chat:index'))
