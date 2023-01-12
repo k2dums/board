8f3516c2b8eb8fa92dfd4fac1846956c5cc8d1e5
k2dums <norbu.paddu@gmail.com>
2023-01-13 01:22:39 +0530

    added a url path for the api

--- chat/urls.py
+++ chat/urls.py
@@ -2,10 +2,17 @@ from django.urls import path
 from . import views
 
 app_name='chat'
+#Based on the settings from the project thus chat/register
 urlpatterns=[
   path('',views.index_view,name='index'),
   path('register/',views.register_view,name='register'),
   path('login/',views.login_view,name='login'),
-  path('logout/',views.logout_view,name='logout')
+  path('logout/',views.logout_view,name='logout'),
+
+
+
+  #api route
+  path('<str:username>/',views.load_chatbox,name='chatbox'),#chat/k2dums
+
 ]
 
