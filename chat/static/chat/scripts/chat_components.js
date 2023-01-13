183d235a0db57bbf1d28a8c310e7e196df69e369
k2dums <norbu.paddu@gmail.com>
2023-01-14 00:21:14 +0530

    fixed the chat_left/right components.The headers for both the chat_left/right components are positioned sticky earlier was positioned fixed. Added the ChatMessageSender/Receiver components.

--- chat/static/chat/scripts/chat_components.js
+++ chat/static/chat/scripts/chat_components.js
@@ -1,10 +1,14 @@
 class Chatbox_Left extends React.Component{
     render(){
         return(
-                <div class="chatbox_left" >
-                    <div style={{backgroundColor: "lightgrey",height:'100vh' }} class="container col-4 p-4">
-                        <div style={{color: "black"}} class="row justify-content-center"><h1>Chatbox</h1></div>
-                        <div class="chats">
+                <div id="chatbox_left"  >
+                    {/* <div class="chatbox_left" style={{backgroundColor: "lightgrey",height:'100%'}} > */}
+                  
+                        <div id='cb_left_header' style={{color: "black",top:'0',backgroundColor:'red',zIndex:'1'}} class="row justify-content-center position-sticky">
+                            <div id='cb_left_title'></div><h1>Chatbox</h1>
+                            
+                        </div>
+                        <div class="chats p-4">
                             <div class="row mb-3">
                                 <div   style={{backgroundColor: "lightblue"}} class="col-5">Username</div>
                                 <div   style={{backgroundColor: "white"}} class="col">Message is this </div>
@@ -22,7 +26,6 @@ class Chatbox_Left extends React.Component{
                                 <div style={{backgroundColor: "white"}} class="col">Message is this </div>
                             </div>
                         </div>
-                    </div>
                 </div>
             
               )
@@ -32,39 +35,73 @@ class Chatbox_Left extends React.Component{
 class Chatbox_Right extends React.Component{
     render(){
         return (
-            <div class="chatbox_right">
-                <div class="container col-5 pb-2" style={{backgroundColor: "lightgrey"}}>
-                    <div class="current_chat_header  row justify-content-center border-bottom border-dark position-fixed pt-2 pr-3 pb-3 align-items-center" style={{width:'41.68%',backgroundColor:'lightgrey'}}>
-                        <div class="col-3 "><img style={{height:'90px',width:'100px',backgroundColor:'white'}}></img></div>
-                        <div class="col ml-3"><h4 stlye={{width:'100%'}}>Current Chat</h4></div>
-                    </div>
-                    <div class='current_chat_message_container' style={{paddingTop:'130px'}} >
-                        <div class="current_chat_message border border-primary p-2 bg-info rounded-pill" > 1 Hi how are you doing</div>
-                        <div class="current_chat_message border border-success mt-3 p-2 bg-success rounded-pill"> 2 I am doing well</div>
-                        <div class="current_chat_message border border-primary mt-3 p-2 bg-info rounded-pill">Hope you are doing well too.</div>
-                        <div class="current_chat_message border border-success mt-3 p-2 bg-success rounded-pill">How is the weather there</div>
-                        <div class="current_chat_message border border-primary mt-3 p-2 bg-info rounded-pill">Hi how are you doing</div>
-                        <div class="current_chat_message border border-success mt-3 p-2 bg-success rounded-pill">I am doing well</div>
-                        <div class="current_chat_message border border-primary mt-3 p-2 bg-info rounded-pill">Hope you are doing well too.</div>
-                        <div class="current_chat_message border border-success mt-3 p-2 bg-success rounded-pill">How is the weather there</div>
-                        <div class="current_chat_message border border-primary mt-3 p-2 bg-info rounded-pill">Hi how are you doing</div>
-                        <div class="current_chat_message border border-success mt-3 p-2 bg-success rounded-pill">I am doing well</div>
-                        <div class="current_chat_message border border-primary mt-3 p-2 bg-info rounded-pill">Hope you are doing well too.</div>
-                        <div class="current_chat_message border border-success mt-3 p-2 bg-success rounded-pill">How is the weather there</div>
-                        <div class="current_chat_message border border-primary mt-3 p-2 bg-info rounded-pill">Hi how are you doing</div>
-                        <div class="current_chat_message border border-success mt-3 p-2 bg-success rounded-pill">I am doing well</div>
-                        <div class="current_chat_message border border-primary mt-3 p-2 bg-info rounded-pill">Hope you are doing well too.</div>
-                        <div class="current_chat_message border border-success mt-3 p-2 bg-success rounded-pill">How is the weather there</div>
-                    </div>
-                    <div class="current_chat_input mt-3 container">
-                        <div class='row'>
-                            <input type='text' class='rounded' placeholder="Enter....." style={{height:'50px',width:'100%'}}></input>
-                        </div>
+            <div id='chatbox_right' class="border-bottom border-dark" style={{height:'100%'}}>
+                <div id="cb_right_header" class="p-3 position-sticky" style={{top:'0',backgroundColor:'lightgrey',}}>
+                    <div class="row ">
+                        <div class="col-3 " id='current_chat_header_dp'><img style={{height:'90px',width:'100px',backgroundColor:'white'}}></img></div>
+                        <div class="col " id='current_chat_header_title'><h4 >Current Chat</h4></div>
                     </div>
                 </div>
+             
+
+            <div class='current_chat_message_container pl-3 pr-3 '  >
+                <ChatMessageSender text='1 Hi how are you doing'/>
+                <ChatMessageReciever text=' 2 I am doing well'/>
+                <ChatMessageSender text='3 Hi how are you doing'/>
+                <ChatMessageReciever text='4  I am doing well'/>
+                <ChatMessageSender text='5 Hi how are you doing'/>
+                <ChatMessageSender text='6 I am doing well'/>
+                <ChatMessageReciever text='7 Hi how are you doing'/>
+                <ChatMessageSender text='8  I am doing well'/>
+                <ChatMessageSender text='9 Hi how are you doing'/>
+                <ChatMessageReciever text='10 Hi how are you doing'/>
+                <ChatMessageSender text='11 I am doing well'/>
+                <ChatMessageReciever text='12 Hi how are you doing'/>
+                <ChatMessageSender text='13 I am doing well'/>
+                <ChatMessageReciever text='14 Hi how are you doing'/>
+                <ChatMessageSender text='15 I am doing well'/>
+            </div>
+
+            <footer class="current_chat_input  p-3 position-sticky mt-3" style={{backgroundColor:'lightgrey',bottom:'0',}}>
+                <input type='text' class='rounded' placeholder="Enter....." style={{height:'50px',width:'100%'}}></input>
+            </footer>
+              
             </div>
         )
     }
 }
 
-ReactDOM.render(<Chatbox_Right/>,document.querySelector("#app"))
+
+class ChatMessageSender extends React.Component{
+    render(){
+        return(        
+        <div class="chat_sender mt-3 row justify-content-end mr-1 ">
+            <div class='d-inline-block  bg-success rounded-pill border border-success  p-2 '>
+               <span class='message '>{this.props.text}</span>
+           </div>
+        </div>
+
+            )
+    }
+}
+
+
+class ChatMessageReciever extends React.Component{
+    render(){
+        return(
+            <div class="chat_receiver mt-3 ">
+                 <div class='d-inline-block  bg-info rounded-pill border border-info  p-2'>
+                    <span class='message'>{this.props.text}</span>
+                </div>
+            </div>
+           
+        )
+    }
+}
+
+
+
+
+
+ReactDOM.render(<Chatbox_Left/>,document.querySelector("#chat_left_container"))
+ReactDOM.render(<Chatbox_Right/>,document.querySelector("#chat_right_container"))
