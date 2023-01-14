369717540187b5876c1cdf554c6087e930b5213f
k2dums <norbu.paddu@gmail.com>
2023-01-15 00:02:21 +0530

    Added the profile view with its path, now the api routes are preceded with api/

--- chat/urls.py
+++ chat/urls.py
@@ -5,6 +5,7 @@ app_name='chat'
 #Based on the settings from the project thus chat/register
 urlpatterns=[
   path('',views.index_view,name='index'),
+  path('<str:username>',views.profile_view,name='profile'),
   path('register/',views.register_view,name='register'),
   path('login/',views.login_view,name='login'),
   path('logout/',views.logout_view,name='logout'),
@@ -12,7 +13,7 @@ urlpatterns=[
 
 
   #api route
-  path('<str:username>/',views.load_chatbox,name='chatbox'),#chat/k2dums
+  path('api/<str:username>/',views.load_chatbox_messages,name='chatbox'),#chat/k2dums
 
 ]
 
