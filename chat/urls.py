eeaad01d063a0ccd1626ecef1672614a14b5fda5
k2dums <norbu.paddu@gmail.com>
2023-01-18 19:42:14 +0530

    pushesd the profile route to the bottom , can cause routing problem like login would be taken as a profile rather than a actual route path to the login page

--- chat/urls.py
+++ chat/urls.py
@@ -5,10 +5,11 @@ app_name='chat'
 #Based on the settings from the project thus chat/register
 urlpatterns=[
   path('',views.index_view,name='index'),
-  path('<str:username>',views.profile_view,name='profile'),
   path('register/',views.register_view,name='register'),
   path('login/',views.login_view,name='login'),
   path('logout/',views.logout_view,name='logout'),
+  path('<str:username>',views.profile_view,name='profile'),
+ 
 
 
 
