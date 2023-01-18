8532f42d91d6bd8bf98bbddc988fa4d3b0c4e4e1
k2dums <norbu.paddu@gmail.com>
2023-01-18 19:39:56 +0530

    registered message to the admin

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import User,Message
+class MessageAdmin(admin.ModelAdmin):
+    list_display=('sender','receiver','message','timestamp')
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Message,MessageAdmin)
