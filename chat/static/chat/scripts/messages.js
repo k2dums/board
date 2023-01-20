ede83bad18929d8c0e6bad7cdabe02400141e0b0
k2dums <norbu.paddu@gmail.com>
2023-01-20 23:54:57 +0530

    the websocket connection is made only when the chatting window is diaplyed on the right . getusers() is used to fetch the recent chat with other users . create_recentChatDiv() displays the the div required for the recent chats on the left coloumn. chatWithUser() makes the websocket connection and fetches all the messages for that user and the other user

--- chat/static/chat/scripts/messages.js
+++ chat/static/chat/scripts/messages.js
@@ -2,14 +2,101 @@
 
 document.addEventListener('DOMContentLoaded',()=>{
 
+getUsers();
+
+});
+
+
+        
+   
+
+//function for displaying the message on the html element
+function display_message(data){
+    const receiver=document.querySelector('#chatting_with_user').innerHTML
+    if (data.sender==username && receiver==data.receiver){
+        var div=render_senderMessage(data.message)
+    }
+    else{
+        var div=render_receiverMessage(data.message)
+    }
+    const chat_container=document.querySelector('#chat_messages').append(div)
+}
+
+
+function render_senderMessage(data){
+    const chat_sender=document.createElement('div');
+    chat_sender.classList.add('chat_sender','mt-3','row','justify-content-end','mr-1');
+    const inner_chat=document.createElement('div');
+    inner_chat.classList.add('d-inline-block','bg-success','rounded-pill','border','border-success','p-2');
+    const message=document.createElement('span');
+    message.innerHTML=data;
+    chat_sender.append(inner_chat);
+    inner_chat.append(message)
+    return chat_sender
+}
+
+function render_receiverMessage(data){
+    const chat_sender=document.createElement('div');
+    chat_sender.classList.add('chat_receiver','mt-3','mr-1');
+    const inner_chat=document.createElement('div');
+    inner_chat.classList.add('d-inline-block',  'bg-info','rounded-pill','border','border-info','p-2');
+    const message=document.createElement('span');
+    message.innerHTML=data;
+    chat_sender.append(inner_chat);
+    inner_chat.append(message)
+    return chat_sender
+}
+
+function getUsers(){
+    div=document.querySelector('#recent_chat_users');
+    fetch(`/chat/api/${username}`,{
+        method:'GET',
+    })
+    .then(response=>response.json())
+    .then(messages=>{
+        messages.forEach(message=>create_recentChatDiv(message,div));
+    });
+}
+
+
+function create_recentChatDiv(message,div){
+    document.querySelector('#chatting_with_container').style.display='none';
+    document.querySelector('#current_chat_input_container').style.display='none';
+    chat_user=document.createElement('div');
+    chat_user.classList.add('chat_user','row','mt-3','mb-1','justify-content-center');
+    chat_user.addEventListener('click',()=>{chatWithUser(message.username)})
+    user_div=document.createElement('div');
+    user_div.classList.add('col-5', 'text-center');
+    user_div.style.background='lightblue';
+    user_div.innerHTML=message.username;
+    message_div=document.createElement('div');
+    message_div.classList.add('col-5', 'text-center');
+    message_div.style.background='white';
+    message_div.innerHTML=message.message;
+    chat_user.append(user_div);
+    chat_user.append(message_div);
+    div.append(chat_user);
+}
+
+// //Recent Chat Users div
+// <div class="chat_user row mt-3 mb-1 justify-content-center" >
+//     <div   style="background-color: lightblue;" class="col-5 text-center">{{user}}</div>
+//     <div   style='background-color: white;' class="col-5">Message... </div>
+// </div> 
 
 
 
+
+function chatWithUser(chatUser){
+document.querySelector('#chatting_with_container').style.display='flex';
+document.querySelector('#current_chat_input_container').style.display='block';
+document.querySelector('#chat_messages').innerHTML=""
+document.querySelector('#chatting_with_user').innerHTML=chatUser
 const roomName='norbu'
-const wsStart='ws://'
-const loc=window.location.host
-const path=`/ws/chat/${roomName}/`
-const endpoint=wsStart+loc+path
+// const wsStart='ws://'
+// const loc=window.location.host
+// const path=`/ws/chat/${roomName}/`
+// const endpoint=wsStart+loc+path
 
 const chatSocket = new WebSocket(
     'ws://'
@@ -18,6 +105,18 @@ const chatSocket = new WebSocket(
     + roomName
     + '/'
 );
+
+
+fetch(`/chat/api/${username}/${chatUser}`)
+.then(response=>response.json())
+.then(messages=>{
+    messages.forEach(message=>{
+        display_message(message);
+    })
+})
+
+
+
 
 //When you get the message do this
 chatSocket.onmessage=function(e){
@@ -57,45 +156,19 @@ chat_input.addEventListener('keypress', (event)=>{
     }
       
   });
-});
-
-
-        
-   
-
-//function for displaying the message on the html element
-function display_message(data){
-    const receiver=document.querySelector('#current_chat_header_receiver').innerHTML
-    if (data.sender==username && receiver==data.receiver){
-        var div=render_senderMessage(data.message)
-    }
-    else{
-        var div=render_receiverMessage(data.message)
-    }
-    const chat_container=document.querySelector('#current_chat_message_container').append(div)
 }
 
 
-function render_senderMessage(data){
-    const chat_sender=document.createElement('div');
-    chat_sender.classList.add('chat_sender','mt-3','row','justify-content-end','mr-1');
-    const inner_chat=document.createElement('div');
-    inner_chat.classList.add('d-inline-block','bg-success','rounded-pill','border','border-success','p-2');
-    const message=document.createElement('span');
-    message.innerHTML=data;
-    chat_sender.append(inner_chat);
-    inner_chat.append(message)
-    return chat_sender
-}
 
-function render_receiverMessage(data){
-    const chat_sender=document.createElement('div');
-    chat_sender.classList.add('chat_sender','mt-3','mr-1');
-    const inner_chat=document.createElement('div');
-    inner_chat.classList.add('d-inline-block',  'bg-info','rounded-pill','border','border-info','p-2');
-    const message=document.createElement('span');
-    message.innerHTML=data;
-    chat_sender.append(inner_chat);
-    inner_chat.append(message)
-    return chat_sender
-}
+
+// <div class="chat_sender mt-3 mb-1 row justify-content-end mr-1 ">
+//     <div class='d-inline-block  bg-success rounded-pill border border-success  p-2 '>
+//        <span class='message '>{this.props.text}</span>
+//     </div>
+//  </div>
+
+//     <div class="chat_receiver mt-3 mb-1">
+//         <div class='d-inline-block  bg-info rounded-pill border border-info  p-2'>
+//            <span class='message'>{this.props.text1}</span>
+//        </div>
+//     </div>  
