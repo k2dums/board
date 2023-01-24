bed125425e11b5e2be768ac9e8fbdaf0623db6ab
k2dums <norbu.paddu@gmail.com>
2023-01-25 03:00:43 +0530

    Added logging,try and catch to database access and sending messages

--- chat/consumers.py
+++ chat/consumers.py
@@ -2,6 +2,9 @@ import json
 from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
 from .models import Message,User
+import logging
+
+logging.basicConfig(level=logging.ERROR)
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
@@ -18,40 +21,52 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     # Receive message from WebSocket
     async def receive(self, text_data):
-        print('I got a message')
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        sender=text_data_json['sender']
-        receiver=text_data_json['receiver']
-        receiver_id=text_data_json['receiver_id']
-        sender_id=text_data_json['sender_id']
-        
-        print({'message':message,'sender':sender,'receiver':receiver,'receiver_id':receiver_id})
+        try:
+            text_data_json = json.loads(text_data)
+            message = text_data_json["message"]
+            sender=text_data_json['sender']
+            receiver=text_data_json['receiver']
+            receiver_id=text_data_json['receiver_id']
+            sender_id=text_data_json['sender_id']
+            logging.info(f'[Received], data from {sender}')
+        except Exception as e:
+            logging.error(f'[Error-Receiving],Traceback: chat.consumer.recieve(),{e}')
+
+        logging.info({'message':message,'sender':sender,'receiver':receiver,'receiver_id':receiver_id})
         receiver_obj=await self.get_user(receiver_id)
         other_room=''
         if receiver_obj:
             other_room="chat_%s" % receiver_id
-            print('other_room',other_room)
         else:
             return None
         sender_obj=await self.get_user(sender_id)
-        # Send message to room group
 
+        # Send message to room group
         await self.save_message({'message':message,'receiver':receiver_obj,'sender':sender_obj})
-        await self.channel_layer.group_send(
-            self.room_group_name,{'type':"chat_message","message":message,'sender':sender,'receiver':receiver}
-        )
-        await self.channel_layer.group_send(
-            other_room, {"type": "chat_message", "message": message,'sender':sender,'receiver':receiver}
-        )
+        
+        try:
+            await self.channel_layer.group_send(
+            self.room_group_name,{'type':"chat_message","message":message,'sender':sender,'receiver':receiver,'receiver_id':receiver_id,'sender_id':sender_id}
+            )
+            await self.channel_layer.group_send(
+                other_room, {"type": "chat_message", "message": message,'sender':sender,'receiver':receiver,'receiver_id':receiver_id,'sender_id':sender_id}
+            )
+            logging.info(f"[Sending], data to {receiver} and {sender}")
+        except Exception as e:
+            logging.error(f'[Error-Sending Messages], Traceback: chat.consumer.recieve(), {e}')
 
     # Receive message from room group
     async def chat_message(self, event):
-        message = event["message"]
-        sender=event['sender']
-        receiver=event['receiver']
-        # Send message to WebSocket
-        await self.send(text_data=json.dumps({'message': message,'receiver':receiver,'sender':sender}))
+        try:
+            message =event["message"]
+            sender=event['sender']
+            receiver=event['receiver']
+            receiver_id=event['receiver_id']
+            sender_id=event['sender_id']
+            # Send message to WebSocket
+            await self.send(text_data=json.dumps({'message': message,'receiver':receiver,'sender':sender,'receiver_id':receiver_id,'sender_id':sender_id}))
+        except Exception as e:
+            logging.error(f'[Error Sending message], Traceback: chat.consumers.chat_message, {e}')
     
 
     @database_sync_to_async
@@ -59,8 +74,9 @@ class ChatConsumer(AsyncWebsocketConsumer):
         try:
             messg_obj=Message(sender=message['sender'],receiver=message['receiver'],message=message['message'])
             messg_obj.save()
+            logging.info(f"[Saved],data from {message['sender']}")
         except Exception as e:
-            print(e)
+            logging.error(f'[Error-Saving message to database],Traceback: chat.consumers.save_message(),{e}')
 
     @database_sync_to_async
     def get_user(self,user_id):
@@ -72,5 +88,5 @@ class ChatConsumer(AsyncWebsocketConsumer):
                 obj = None
             return obj
         except Exception as e:
-            print(e)
+            logging.error(f'[Error-Getting user from database],Traceback: chat.consumer.get_user(),{e}')
        
