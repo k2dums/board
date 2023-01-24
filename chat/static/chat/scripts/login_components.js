60495a6ec4f8c862c89ee4f7c6b6bf6b422e0433
k2dums <norbu.paddu@gmail.com>
2023-01-25 03:07:43 +0530

    changed the type to text and now takes username,changed password id

--- chat/static/chat/scripts/login_components.js
+++ chat/static/chat/scripts/login_components.js
@@ -5,18 +5,15 @@ class Login extends React.Component{
             <div class="row justify-content-center">
                 <h1>Login</h1>
             </div>
-            <form>
             <div class="mb-3">
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Email Address"/>
+              <input type="text" class="form-control" id="login_username" name='username' aria-describedby="emailHelp" placeholder="Username"/>
             </div>
             <div class="mb-3">
-              <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+              <input type="password" class="form-control" id="login_password" name='password' placeholder="Password"/>
             </div>
             <div class="row justify-content-center">
             <button type="submit" class="btn btn-primary">Login</button>
             </div>
-       
-          </form>
             </div>
        
         
