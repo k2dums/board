1df385fd21ed20e31fde1819ca4732984d0ed207
k2dums <norbu.paddu@gmail.com>
2023-01-28 00:40:42 +0530

    added id to the MessageAdmin

--- chat/admin.py
+++ chat/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import User,Message
 class MessageAdmin(admin.ModelAdmin):
-    list_display=('sender','receiver','message','timestamp')
+    list_display=('id','sender','receiver','message','timestamp','received','sent','read')
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Message,MessageAdmin)
