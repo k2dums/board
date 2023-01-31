70f828e85f3980eeb09a6f91734de273b5573d6c
k2dums <norbu.paddu@gmail.com>
2023-01-31 22:16:59 +0530

    now user in the admin page display id,username

--- chat/admin.py
+++ chat/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from .models import User,Message
 class MessageAdmin(admin.ModelAdmin):
     list_display=('id','sender','receiver','message','timestamp','received','sent','read')
+
+class UserAdmin(admin.ModelAdmin):
+    list_display=('id','username')
 # Register your models here.
-admin.site.register(User)
+admin.site.register(User,UserAdmin)
 admin.site.register(Message,MessageAdmin)
